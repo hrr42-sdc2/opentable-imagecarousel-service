bb6d22fe184dda3bb1d71970146ded886a47b7c5
Dan Xu <danxu@dans-mbp.lan>
2019-12-12 01:11:52 -0800

    change bundle.js to s3 link

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.set('useCreateIndex', true);
+// mongoose.set('useCreateIndex', true);
 var db = mongoose.connect('mongodb://localhost/oc-imageCarousel', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
