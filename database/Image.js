a507b5cf2f842ef90467c5bc6ea0e607f794937c
Eivin Adlawan <eadlawa2@gmail.com>
2019-12-31 12:02:55 -0600

    Savepoint: still working on delete

--- database/Image.js
+++ database/Image.js
@@ -21,18 +21,109 @@ var findById = (restaurantId) => {
     .exec();
 };
 
-//! Original above ^^^
+//? Find all photos for a certain restaurantId
+var findRestaurantAndPhoto = (restaurantId, photoId) => {
+  console.log(`This is the restaurantId: ${restaurantId}`)
+  console.log(`This is the photoId: ${photoId}`)
 
-var addPicture = (pictureId) => {
-  return Image.create(pictureId);
+  return Image.find({
+
+    restaurantId: restaurantId,
+    photoId: photoId})
+  // .exec();
+    .then(result => res.json(result));
 }
 
-var updatePicture = function (imageId, newImageId) {
+
+//! Original above ^^^
+// use photoId in line 7 instead of pictureId?
+var addPicture = ({samplePhotos}) => {
+  return Image.create({samplePhotos});
+}
+
+
+
+var updatePicture = function (originalPhotoId, data) {
+  return Image.findByIdAndUpdate(
+    originalPhotoId,
+    data
+  )
+
+  console.log('This is the originalPhotoId: ', originalPhotoId)
+  console.log('This is the data: ', data)
   return Image.update(
-    { id: {$gt: imageId} },
-    { $set: {id: newImageId} },
-    { multi: true }
-  );
+    // { restaurantId: {$gt: photoId} },
+    { photoId: originalPhotoId },
+    data
+    // { $set: {photoId: newImageId} },
+    // { multi: true }
+  )
+
+
+  // const updatePicture = Image.update(
+  //   { id: {$gt: imageId} }, //find get
+  //   { $set: {id: newImageId} },
+  //   { multi: true }
+  // )
+  // return updatePicture;
+
+
+  // var image = Image();
+  // const updateOnePicture = image.updateOne(
+  //   { id: {$gt: imageId} },
+  //   { $set: {id: newImageId} },
+  //   { multi: true }
+  // )
+  // return updateOnePicture;
+
 };
+
+//!! RON'S ADDITION FOR THE PUT REQUEST TO UPDATE THE RESERVATION
+//format is: findByIdAndUpdate(id, {update}, callback)
+//?
+// updateReservation = (booking, callback) => {
+//   //console.log("this is the booking:", booking);
+//   console.log(booking._id);
+//   let query = Reservation.findByIdAndUpdate(
+//     booking._id,
+//     { guests: booking.guests },
+//     function(err, result) {
+//       if (err) {
+//         return err;
+//       } else {
+//         return result;
+//       }
+//     }
+//   );
+//   return query.exec();
+// };
+
+//! this my postman req.body
+//?
+// {
+// "_id":"5dfaed3c3144ba52b6c2b2b4",
+// "guests":22
+
+// }
+
+//!! END OF THE CHANGE FOR THE PUT REQUEST
+
+var deletePicture = function (originalPhotoId, data) {
+  // return Image.deleteOne(
+  //   originalPhotoId,
+  //   data
+  // )
+
+  console.log('This is the originalPhotoId: ', originalPhotoId)
+  console.log('This is the data: ', data)
+  return Image.deleteOne(
+    // { restaurantId: {$gt: photoId} },
+    { photoId: originalPhotoId },
+    data
+    // { $set: {photoId: newImageId} },
+    // { multi: true }
+  )
+}
+
 // line 30 may need work $set is not setting the new imageId
-module.exports = { Image, addPicture, updatePicture, findById };
+module.exports = { Image, findRestaurantAndPhoto, addPicture, updatePicture, findById };
--- server/index.js
+++ server/index.js
@@ -26,24 +26,49 @@ app.get('/restaurantid/:id', (req, res) => {
     });
 });
 
+
+//? Get all photos for a certain restaurantId
+app.get('/restaurantid/:id/:photoId', (req, res) => {
+  const restaurantId = Number(req.params.id);
+  const photoId = Number(req.params.photoId);
+  db.findRestaurantAndPhoto(restaurantId, photoId)
+    .then((data) => {
+      res.send(JSON.stringify(data));
+    })
+    .catch((err) => {
+      res.end();
+    });
+});
+
+
 app.post('/restaurantid/:id', function(req, res) {
-  db.addPicture(req.params.id)
-  .then((data) => {
+  db.addPicture(req.body) //send request to db
+  .then((data) => { //receive data back from db
+    //send success status if succesful
+
     res.status(201).send('Picture added!');
   })
+    //catch error if failure
     .catch((err) => {
       res.status(500).end()
     })
 });
 
-app.put('/restaurant/:id/:idToBeUpdated', (req, res) => {
-  const restaurantId = Number(req.params.id);
-  const idToBeUpdated = Number(req.params.idToBeUpdated);
-  const data = req.body;
-  console.log(restaurantId, idToBeUpdated);
-  db.updatePicture(restaurantId, idToBeUpdated)
+// app.put('/restaurantid/:id/:idToBeUpdated', (req, res) => {
+  app.put('/restaurantid/', (req, res) => {
+  // const restaurantId = Number(req.params.id);
+  // const idToBeUpdated = Number(req.params.idToBeUpdated);
+  const {_id, ...rest} = req.body;
+
+  // console.log(`Original restaurantId: null, and new ID to replace the Original: ${idToBeUpdated}`);
+  console.log('this is the UPDATED req.body : ', rest)
+  console.log(`Original restaurantId: ${_id}`);
+  db.updatePicture(_id, rest)
+  //server is requesting the database using db.updatePicture request to get the imageId using
     .then((data) => {
-      console.log(data);
+      // console.log('Status, Showing number \'n\' of documents matching restaurantId, number of documents nModified: ',data);
+
+      console.log('This shows the modified photo with all of its details: ',data);
       res.status(200).send('Picture updated!')
     })
     .catch ((err) => {
@@ -52,8 +77,56 @@ app.put('/restaurant/:id/:idToBeUpdated', (req, res) => {
   })
 })
 
+//!! RON'S ADDITION FOR THE PUT REQUEST TO UPDATE THE RESERVATION
+//PUT is update: mongo _id field is included in the object res, use
+//Maybe use:
+//https://docs.mongodb.com/manual/reference/method/db.collection.replaceOne/
+//or https://docs.mongodb.com/manual/reference/method/db.collection.update/
+//db.collection.replaceOne(), or db.collection.update()
+//?
+// app.put('/reservation', function (req, res) {
+//   const booking = req.body;
+//   console.log(booking);
+//   //const time = req.params.restaurant_time;
+//   // res.send(booking)
+//   Reservation.updateReservation(booking)
+//     .then((notification) => {
+//       // console.log(notification);
+//       // console.log(booking);
+//       res.write(JSON.stringify(notification));
+//       res.end();
+//     })
+//     .catch((err) => {
+//       console.log('Error occurred: ', err);
+//       res.status(500).send(new Error(err));
+//       res.end();
+//     });
+//   // res.send("Updated!");
+// });
 
+//!! END OF PUT REQUEST ADDED BY RON
 
+app.delete('/restaurantid/', (req, res) => {
+  // const restaurantId = Number(req.params.id);
+  // const idToBeUpdated = Number(req.params.idToBeUpdated);
+  const {_id, ...rest} = req.body;
+
+  // console.log(`Original restaurantId: null, and new ID to replace the Original: ${idToBeUpdated}`);
+  console.log('this is the UPDATED req.body : ', rest)
+  console.log(`Original restaurantId: ${_id}`);
+  db.delete(_id, rest)
+  //server is requesting the database using db.updatePicture request to get the imageId using
+    .then((data) => {
+      // console.log('Status, Showing number \'n\' of documents matching restaurantId, number of documents nModified: ',data);
+
+      console.log('This shows the deleted photo with all of its details: ',data);
+      res.status(200).send('Poof! Picture deleted!')
+    })
+    .catch ((err) => {
+    console.log(err)
+    res.status(404);
+  })
+})
 
 app.listen(port, () => {
   console.log(`Port ${port} is listening`);
