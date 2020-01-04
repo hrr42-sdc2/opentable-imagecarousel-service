08a37a44dbe1344ff28fdb02207b66793896447d
Eivin Adlawan <eadlawa2@gmail.com>
2020-01-04 05:32:12 -0600

    Savepoint: trying to create my own fake data but it's not populating my CSV file. Will try again after I get more sleep.e

--- server/db/data/postgresDataGenerator.js
+++ server/db/data/postgresDataGenerator.js
@@ -1,3 +1,5 @@
+var faker = require('faker');
+
 var csvWriter = require('csv-write-stream')
 // var writer = csvWriter()
 
@@ -11,22 +13,64 @@ const fs = require('fs');
 // // Writing more now is not allowed!
 
 //! Here is the test script:
+// // csvWriter has been loaded with the: require('csv-write-stream')
+// var writer = csvWriter({ headers: ["hello", "foo"]})
+// // writer.pipe(fs.createWriteStream('/Users/eivinadlawan/hrr42/sdc-image-carousel/opentable-imagecarousel-service/server/db/data/example.csv'))
 
-var writer = csvWriter({ headers: ["hello", "foo"]})
-// writer.pipe(fs.createWriteStream('/Users/eivinadlawan/hrr42/sdc-image-carousel/opentable-imagecarousel-service/server/db/data/example.csv'))
-writer.pipe(fs.createWriteStream(__dirname + '/example.csv'))
-writer.write(['world3', 'bar3'])
-writer.end()
+
+// writer.pipe(fs.createWriteStream(__dirname + '/example.csv'))
+// writer.write(['world2', 'bar2'])
+// writer.end()
 
 // produces: hello,foo\nworld,bar\n
 
 //------------------
 //! Here is the real script bellow :
 
-var writer = csvWriter({ headers: ["id", "restaurantId", "title"]})
-// writer.pipe(fs.createWriteStream('/Users/eivinadlawan/hrr42/sdc-image-carousel/opentable-imagecarousel-service/server/db/data/example.csv'))
-writer.pipe(fs.createWriteStream(__dirname + '/example.csv'))
-writer.write(['world2', 'bar2'])
-writer.end()
+// var writer = csvWriter({ headers: ["id", "restaurantId", "title"]})
+// // writer.pipe(fs.createWriteStream('/Users/eivinadlawan/hrr42/sdc-image-carousel/opentable-imagecarousel-service/server/db/data/example.csv'))
+// writer.pipe(fs.createWriteStream(__dirname + '/example.csv'))
+// writer.write(['world2', 'bar2'])
+// writer.end()
 
 // produces: hello,foo\nworld,bar\n
+
+//!
+
+// this loads imageCarousel with: require('csv-write-stream')
+var imageCarousel = csvWriter();
+
+
+
+// generate 50 restaurants
+
+var restaurantTotal = 50;
+
+// var columnsForCarousel = {'id', 'restaurantId', 'title'};
+
+// get a random integer between two values, inclusive
+var randomizeNum = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
+
+
+
+
+var columnsForCarousel = (id) => {
+  return imageCarousel.write({
+    unique_Id: id,
+    restaurant_id: randomizeNum(1, 50),
+    image_title: faker.commerce.productName()
+  })
+
+};
+
+imageCarousel.pipe(fs.createWriteStream(__dirname + '/example.csv'))
+var id = 1;
+
+
+
+imageCarousel.end()
